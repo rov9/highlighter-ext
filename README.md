462f32a5fb169fa05b542ef69bae32d2c598921f
abhijeet <abhijeetsinha43@gmail.com>
2023-01-09 18:35:57 +0530

    highlighter fixed working files

---
+++ README.md
@@ -0,0 +1,2 @@
+# highlighter
+It is a Chrome extension that highlights any text on a web page on internet
---
+++ popup.js
@@ -0,0 +1,23 @@
+// Get the highlight button
+const highlightButton = document.getElementById("highlight-button");
+// Add an event listener to the button
+highlightButton.addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: highlightText,
+  });
+});
+
+function highlightText() {
+  const selectedText = window.getSelection().toString();
+  let innerText = window.getSelection().anchorNode.parentElement.innerHTML;
+
+  const highlightedText = innerText.replace(
+    selectedText,
+    `<span style="display:inline-block;background-color:yellow">${selectedText}</span>`
+  );
+
+  window.getSelection().anchorNode.parentElement.innerHTML = highlightedText;
+}
